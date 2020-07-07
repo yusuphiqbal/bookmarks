22ee45ee2b1f236c1e446288df15fd2a1dad7d91
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-07 10:42:15 +0300

    Added login and logout url settings for the project

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Authentication URLs
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
