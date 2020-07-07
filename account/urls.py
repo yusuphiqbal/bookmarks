a13cf9dec872c96870bbfb275b43f963f4e01aa4
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-07 22:25:14 +0300

    Adde Django authentication urls for resetting password

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .forms import LoginForm, CustomAuthenticationForm, CustomPasswordChangeForm
+from .forms import LoginForm, CustomAuthenticationForm, CustomPasswordChangeForm, CustomPasswordResetForm, CustomSetPasswordForm
 
 
 urlpatterns = [
@@ -11,4 +11,9 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('password_change/', auth_views.PasswordChangeView.as_view(form_class=CustomPasswordChangeForm), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # Reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(form_class=CustomPasswordResetForm), name='password_reset'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(form_class=CustomSetPasswordForm), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
