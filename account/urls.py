c8dd497f9f7d5e612adc0c48e4ec13902c64e21f
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-07 08:29:13 +0300

    Added url pattern for login that uses Django's LoginView

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm, CustomAuthenticationForm
 
 
 urlpatterns = [
-    # post views
-    path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(authentication_form=CustomAuthenticationForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
