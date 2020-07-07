bb09daa716f451a9f520f27560a5468d9a50267e
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-07 22:30:43 +0300

    Added email backend setting for Django to log emails to the console

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+
+# Email settings.
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
