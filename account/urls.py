a51981b68b61833b970d1478a0a4efbf1cb65899
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-07 21:34:22 +0300

    Added urls for password_change_form and password_change_done templates

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .forms import LoginForm, CustomAuthenticationForm
+from .forms import LoginForm, CustomAuthenticationForm, CustomPasswordChangeForm
 
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(authentication_form=CustomAuthenticationForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(form_class=CustomPasswordChangeForm), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
