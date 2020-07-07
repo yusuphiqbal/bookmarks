bb57b94a550fa6f193b8f181f17bd12cd2f71c07
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-07 08:27:22 +0300

    Created custom authentication form for login view

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
