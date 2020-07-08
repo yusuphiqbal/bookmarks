aad868c9edbfbdde89ebfb57e0d6e675e05d1374
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-08 11:04:15 +0300

    Added a view to display and process the registration form

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 @login_required
@@ -28,3 +28,17 @@ def user_login(request):
         form = LoginForm()
         
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    
+    return render(request, 'account/register.html', {'form': user_form})
