2e5663a3e5174a60b5469f215debafdc2586b06b
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-08 21:06:31 +0300

    Fixed typo in import statement

--- images/views.py
+++ images/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from forms import ImageCreateForm
+from .forms import ImageCreateForm
 
 
 @login_required
