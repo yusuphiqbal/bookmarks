d46decd1d616c6f4c3be16caff3a340b2f3e7c69
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-07-08 16:05:08 +0300

    Added custom email authentication backend to AUTHENTICATION_BACKENDS list

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -138,3 +138,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# Authentication backends
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
